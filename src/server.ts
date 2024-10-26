65145b3b432a665e1ed0888a107946ac95a99c3a
Jessica Gondim <jessicagbsg@gmail.com>
2024-10-26 14:06:54 -0300

    chore: setup sequelize

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-
+import db from "./config/database.config";
 dotenv.config();
+
+db.authenticate()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("Error: " + err);
+  });
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
